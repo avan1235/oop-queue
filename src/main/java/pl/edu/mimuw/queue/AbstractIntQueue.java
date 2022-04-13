828372fa7dfc35a9c119298f50db1921f5b6b416
Artur Kamieniecki <kamart02@wp.pl>
2022-04-13 11:30:01 +0200

    Finished implementation

--- src/main/java/pl/edu/mimuw/queue/AbstractIntQueue.java
+++ src/main/java/pl/edu/mimuw/queue/AbstractIntQueue.java
@@ -13,7 +13,7 @@ public abstract class AbstractIntQueue {
   protected int size = 0;
 
   /**
-   * Adds element to the queue based on the queue type.
+   * Adds element to the queue.
    *
    * @throws NullPointerException if the specified element is null
    */
@@ -23,47 +23,16 @@ public abstract class AbstractIntQueue {
    * @return the head of this queue, or {@code null} if this queue is empty
    * and don't remove the element from the queue
    */
-  public Integer peek() {
-    return this.start == null ? null : this.start.getValue();
-  }
+  public abstract Integer peek();
 
   /**
    * @return the head of this queue, or {@code null} if this queue is empty
    * and remove the element from the queue
    */
-  public Integer poll() {
-    if (this.start == null) {
-      return null;
-    }
-
-    Integer returnValue = this.start.getValue();
-    this.start = this.start.getNext();
-    this.size--;
-    if (this.size == 0) {
-      this.end = null;
-    }
-    return returnValue;
-  }
+  public abstract Integer poll();
 
   /**
-   * @return readable representation of queue based on the order of elements in queue
-   */
-  protected String stringQueueRepresentation() {
-    var sb = new StringBuilder();
-    sb.append("[");
-    for (var element = this.start; element != null; element = element.getNext()) {
-      sb.append(element.getValue());
-      if (element.getNext() != null) {
-        sb.append(", ");
-      }
-    }
-    sb.append("]");
-
-    return sb.toString();
-  }
-
-  /**
-   * @return readable representation of ordered queue elements with specified queue type
+   * @return readable representation of ordered queue elements
    */
   @Override
   public abstract String toString();
@@ -71,7 +40,5 @@ public abstract class AbstractIntQueue {
   /**
    * @return the number of elements in this queue
    */
-  public int size() {
-    return this.size;
-  }
+  public abstract int size();
 }
